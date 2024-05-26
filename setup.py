d6c50c39a3a6eebbab990592ca2a61bcfbf00642
pyda-lohith <lohi.pyda@gmail.com>
2024-05-26 19:15:00 +0530

    setup files update

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical GenAI Chatbot',
+    version= '0.0.1',
+    author= 'Lohith Pyda',
+    author_email= 'lohi.pyda@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
